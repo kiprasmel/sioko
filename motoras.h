edcbe39850c385c4474e8955b750cf261732ff71
Kipras Melnikovas <sarpixo@gmail.com>
2019-02-09 21:29:43 +0200

    Remake the `motor` function & clean things up
    Fix a bug where a motor would be set to speed `255-x` instead of `-x`
    whenever provided with a negative speed
    
    And more.
    
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

---
+++ motoras.h
@@ -0,0 +1,11 @@
+#pragma once
+
+typedef enum
+{
+	kryptisPirmyn = 0,
+	kryptisAtgal = 1
+} VaziavimoKryptys;
+
+int kokiaKryptis(int);
+void motor(int, int);
+void motoraiSuLaiku(int, int, unsigned long);
--- motoras.ino
+++ motoras.ino
@@ -1,66 +1,39 @@
-// TODO CHECK - galbūt reiktų ir "stop" krypties?
-enum vaziavimoKryptys
-{
-	kryptisPirmyn = 0,
-	kryptisAtgal = 1,
-};
+#include "motoras.h"
 
 /** jeigu motoroGreitis < 0, kryptis atgal (1), o jeigu >= 0, tai kryptis pirmyn (0). Returnina `int`! */
-int kokiaKryptis(int &motoroGreitis)
+int kokiaKryptis(int motoroGreitis)
 {
-	return motoroGreitis < 0 ? kryptisAtgal : kryptisPirmyn;
+	return motoroGreitis < 0 ? VaziavimoKryptys::kryptisAtgal : VaziavimoKryptys::kryptisPirmyn;
+	// return motoroGreitis < 0 ? 1 : 0;
 }
 
-void motor(int a, int b)
+void motor(int kairioMotoroGreitis, int desinioMotoroGreitis)
 {
 	// `constrain`: jei a > 255 arba < -255, tai returnins minimalią / maximalią reikšme (255 arba -255)
-	a = constrain(a, -255, 255);
-	b = constrain(b, -255, 255);
+	kairioMotoroGreitis = constrain(kairioMotoroGreitis, -255, 255);
+	desinioMotoroGreitis = constrain(desinioMotoroGreitis, -255, 255);
 
-	if (digitalRead(START_MODULE) == LOW) // TODO CHECK ką šitas daro?
+	// Jeigu žema įtampa, sustabdom motorus.
+	if (digitalRead(START_MODULE) == LOW)
 	{
-		a = 0;
-		b = 0;
+		kairioMotoroGreitis = 0;
+		desinioMotoroGreitis = 0;
 	}
 
-	digitalWrite(DIR1, kokiaKryptis(a));
-	analogWrite(PWM1, a); // TODO CHECK WARN patikrink #blogasSkaiciavimas
+	analogWrite(PWM1, kairioMotoroGreitis); // greitis
+	digitalWrite(DIR1, kokiaKryptis(kairioMotoroGreitis));
 
-	// TODO CHECK FIX WARN Kas čia? Kodėl naudojat `255 - abs(a)`? Gausit atvirkštinį skaičių gi! #blogasSkaiciavimas
-	// if (a <= -1)
-	// {
-	// 	digitalWrite(DIR1, 1);
-	// 	analogWrite(PWM1, (255 - abs(a)));
-	// }
-	// else if (a >= 1)
-	// {
-	// 	digitalWrite(DIR1, 0);
-	// 	analogWrite(PWM1, a);
-	// }
-	// else if (a == 0)
-	// {
-	// 	digitalWrite(DIR1, 0);
-	// 	analogWrite(PWM1, 0);
-	// }
+	analogWrite(PWM2, desinioMotoroGreitis); // greitis
+	digitalWrite(DIR2, kokiaKryptis(desinioMotoroGreitis));
+}
 
-	digitalWrite(DIR2, kokiaKryptis(b));
-	analogWrite(PWM2, b); // TODO CHECK WARN patikrink #blogasSkaiciavimas
-
-	// TODO CHECK WARN #blogasSkaiciavimas
-	// if (b <= -1) //&& b < 255
-	// {
-	// 	digitalWrite(DIR2, 1);
-	// 	analogWrite(PWM2, (255 - abs(b)));
-	// }
-	// else if (b >= 1) //&& b > -255
-	// {
-	// 	// b = map(abs(b), 0,255,255,0);
-	// 	digitalWrite(DIR2, 0);
-	// 	analogWrite(PWM2, b); // 255 -
-	// }
-	// else if (b == 0)
-	// {
-	// 	digitalWrite(DIR2, 0);
-	// 	analogWrite(PWM2, 0);
-	// }
+/** naudoja while ciklą, kad motorai vykdytų dalykus X laiko */
+void motoraiSuLaiku(int kairioMotoroGreitis, int desinioMotoroGreitis, unsigned long kiekMsLaukti)
+{
+	time = millis();
+	motor(kairioMotoroGreitis, desinioMotoroGreitis);
+	while (millis() - time <= kiekMsLaukti)
+	{
+		// pause programm (not good). Much like using a delay.
+	}
 }
