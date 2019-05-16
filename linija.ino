f23449e094b5b3eda44ba942b571381bdc956d74
Kipras Melnikovas <sarpixo@gmail.com>
2019-05-16 20:21:01 +0300

    Create an updated line check fn (needs testing)
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

--- linija.ino
+++ linija.ino
@@ -1,6 +1,30 @@
 #include "linija.h"
 #include "inicializuoti.h"
 
+/**
+ * WARNING 
+ * Not fully working yet (needs more testing)
+ */
+void linijosTikrinimas()
+{
+	if (myLINE == 0b0011)
+	{
+		motoraiSuLaiku(-SPEED_GO_BACK_LINE, -SPEED_GO_BACK_LINE, TIME_FOR_GO_BACK_1);
+		motoraiSuLaiku(-SPEED_GO_BACK_LINE_TURN, SPEED_GO_BACK_LINE_TURN2, TIME_FOR_GO_BACK_1);
+	}
+
+	else if (myLINE == 0b1100)
+	{
+		motoraiSuLaiku(-SPEED_GO_BACK_LINE, -SPEED_GO_BACK_LINE, TIME_FOR_GO_BACK);
+		motoraiSuLaiku(SPEED_GO_BACK_LINE_TURN, -SPEED_GO_BACK_LINE_TURN2, TIME_FOR_GO_BACK_1);
+	}
+	else if (myLINE == 0b1111)
+	{
+		motoraiSuLaiku(-SPEED_GO_BACK_LINE, -SPEED_GO_BACK_LINE, TIME_FOR_GO_BACK);
+		motoraiSuLaiku(-SPEED_GO_BACK_LINE_TURN, SPEED_GO_BACK_LINE_TURN2, TIME_FOR_GO_BACK); // nereik TIME_FOR_GO_BACK_1?
+	}
+}
+
 void originaliLinijaBeDefaultCase()
 {
   //Back = false;
