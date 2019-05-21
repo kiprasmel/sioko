db59e6df6805551600560519fbc22ee8322ee52b
Kipras Melnikovas <sarpixo@gmail.com>
2019-05-21 21:00:35 +0300

    Split up `oponentoPozicijosBalas` into 2 functions
    1 calculates the average direction points (like before),
    and the 2nd calculates the median (more accurate - we'll use it from now
    on.)
    
    Signed-off-by: Kipras Melnikovas <sarpixo@gmail.com>

--- kurYraOponentas.ino
+++ kurYraOponentas.ino
@@ -16,120 +16,56 @@
 // 	return ((maxReiksme - minReiksme) * (skaicius - minReiksme) / (maxReiksme - minReiksme) + minReiksme);
 // }
 
-double oponentoPozicijosBalas()
+double oponentoPozicijosBalasVidutinis()
 {
-	double baluSuma = 0;
-	// #TODO pertvarkyt kiekPinu
-	// const double kiekPinu = sizeof(visiPinaiIrJuTeikiamiBalai) / sizeof(visiPinaiIrJuTeikiamiBalai[0][0]);
-	// const double kiekPinu = 9;
-	// #TODO #WARN #CHECK ar reikia dalinti iš 1D, ar iš 2D masyvo `sizeof`o (reiktų iš `const double`)
-
-	const int kiekPinu = 9;
-	// const double tempPinaiIrBalai[][2] = {
-	// 		{Left1, -42.5},
-	// 		{Left2, -85},
-	// 		{Left3, -127.5},
-	// 		{Middle1, 0},
-	// 		{Middle2, 0},
-	// 		{Middle3, 0},
-	// 		{Right1, 42.5},
-	// 		{Right2, 85},
-	// 		{Right3, 127.5}
-	//};
-
-
-	// least important
-	// middle
-	// most important
-
-	// Left3 -> mažiausias (apie 30 laipsnių) aukščiausiai
-	// Left1 -> vidurinis (aukščiau už 2)
-	// Left2 -> 90 laipsniu pats žemiausias
-
-	// Right3 -> aukščiausias (apie 30)
-	// Right1 -> vidurinis
-	// Right2 -> zemiausias (90 laipsniu desine)
-
-	// Middle1 -> left most (kairiausias)
-	// Middle2 -> middle middle
-	// Middle3 -> right most
-
-	// const byte temp = Middle3;
-	// if (digitalRead(temp) == 0)
-	// {
-	// 	digitalWrite(LEDas, HIGH);
-	// }
-	// else
-	// {
-	// 	digitalWrite(LEDas, LOW);
-	// }
-
-	// Serial.print("\nMiddle3 = ");
-	// Serial.print(digitalRead(temp));
-	// Serial.print("  ; digitalRead(Left1) = ");
-	// Serial.print(digitalRead(Left1));
-
-	// const int tempPinaiIrBalai[][2] = {
-	// 		{Left1, 0},		// 0 // -15
-	// 		{Left2, -30}, ////// 64
-	// 		{Left3, -40}, ////// 100
-	// 		{Middle1, 0},
-	// 		{Middle2, 0},
-	// 		{Middle3, 0},
-	// 		{Right1, 0},	 // 0 // 15
-	// 		{Right2, 30},	////// 64
-	// 		{Right3, 40}}; ////// 100
-
-
-	// maximalusBalas = 100;
-	// jeigu imtume laipsniais, tai reiktų max balą daryt 90.
-	// const double tempPinaiIrBalai[][2] = {
-	// 		{Left3, -100},
-	// 		{Left2, -50},
-	// 		{Left1, -15},
-	// 		{Middle1, 0},
-	// 		{Middle2, 0},
-	// 		{Middle3, 0},
-	// 		{Right1, 15},	
-	// 		{Right2, 50},
-	// 		{Right3, 100}
-	// 	};
-
-	// // // iškart grąžinam 0;
-	// // if (arVidurysKaNorsMato())
-	// // {
-	// // 	return 0;
-	// // }
-	
-	/**
-	 * Pirma susirašom matančių pin'ų reikšmes,
-	 * o tada apskaičiuojam medianą.
-	 */
-
 	int kiekPinuMato = 0;
-	double matanciuPinuReiksmes[kiekPinu];
 
-	for (int i = 0; i < kiekPinu; ++i)
+	double baluSuma = 0;
+
+	for (int i = 0; i < kiekYraPinu; ++i)
 	{
 		if (digitalRead(pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[i][0]) == 0) // jeigu matome
 		{
-			// // baluSuma += pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[i][1];
-
-			/**
-			 * [kiekPinuMato] - kiek kol kas šiame cikle mato pin'ų.
-			 * 
-			 * Tai reiškia, jog jei tik pradėjom tikrinti,
-			 * gali būti, jog atsiras ir daugiau matančių.
-			 * 
-			 * Čia realiai naudojama kaip std::vector'o push_back() metodas,
-			 * tik patiems atnaujinant indeksą (kiekPinuMato)
-			 */
-			matanciuPinuReiksmes[kiekPinuMato] = pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[i][1];
-			kiekPinuMato += 1;
+			baluSuma += pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[i][1];
+			++kiekPinuMato;
 		}
 	}
 
-	// // baluSuma /= kiekPinuMato;
+	/** vidurkis */
+	baluSuma /= kiekPinuMato;
+
+	return baluSuma;
+}
+
+double oponentoPozicijosBalasMediana()
+{
+	int kiekPinuMato = 0;
+
+	double baluSuma = 0;
+	int matanciuPinuReiksmes[kiekYraPinu];
+
+	/**
+	 * Pirma susirašom matančių pin'ų reikšmes,
+	 * o tada apskaičiuojam medianą.
+	*/
+
+	for (int i = 0; i < kiekYraPinu; ++i)
+	{
+		if (digitalRead(pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[i][0]) == 0) // jeigu matome
+		{
+			/**
+ 			  * [kiekPinuMato] - kiek kol kas šiame cikle mato pin'ų.
+			  * 
+			  * Tai reiškia, jog jei tik pradėjom tikrinti,
+			  * gali būti, jog atsiras ir daugiau matančių.
+			  * 
+			  * Čia realiai naudojama kaip std::vector'o push_back() metodas,
+			  * tik patiems atnaujinant indeksą (kiekPinuMato)
+			 */
+			matanciuPinuReiksmes[kiekPinuMato] = pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[i][1];
+			++kiekPinuMato;
+		}
+	}
 
 	// 0 1 2 3
 	// (1 + 2) / 2  =  3 / 2  =  1.5
@@ -139,19 +75,11 @@ double oponentoPozicijosBalas()
 	}
 
 	// 0 1 2
-	// 1
+	// = 1
 	else
 	{
 		baluSuma = matanciuPinuReiksmes[kiekPinuMato / 2];
 	}
-
-	// for (int i = 0; i < kiekPinu; ++i)
-	// {
-	// 	if (digitalRead(visiPinaiIrJuTeikiamiBalai[i][0]) == 0) // jeigu matome
-	// 	{
-	// 		baluSuma += visiPinaiIrJuTeikiamiBalai[i][1];
-	// 	}
-	// }
 
 	return baluSuma;
 }
@@ -163,7 +91,7 @@ double oponentoPozicijosBalas()
 */
 double kurYraOponentas() // nuo -1 iki +1
 {
-	return oponentoPozicijosBalas() / maximalusBalas;
+	return oponentoPozicijosBalasMediana() / maximalusBalas;
 }
 
 /** 
