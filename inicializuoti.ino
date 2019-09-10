a343fb2b3de948868bbdb04968f058782c83a341
Kipras Melnikovas <kipras@kipras.org>
2019-09-10 17:47:00 +0300

    Move general config preparation from `setup` into `inicializuoti`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- inicializuoti.ino
+++ inicializuoti.ino
@@ -17,6 +17,8 @@ void inicializuoti() {
 	sukurtiBendraPinuMasyva();
 
 	suteiktiPinModusViskam();
+
+	paruostiGeneralConfiga();
 }
 
 void sukurtiBendraPinuMasyva() {
@@ -38,7 +40,7 @@ void sukurtiBendraPinuMasyva() {
 		pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[bendrasIndex][1] = ziurintysIViduriSensoriai[i][1];
 
 		bendrasIndex++;
-}
+	}
 
 	/** kairę */
 	for (int i = 0; i < ziurintysIKairePinaiDydis; i++) {
@@ -72,4 +74,47 @@ void suteiktiPinModusViskam() {
 	suteiktiPinModeMasyvui(ziurintysIKairePinai, ziurintysIKairePinaiDydis, INPUT);
 	suteiktiPinModeMasyvui(ziurintysIViduriPinai, ziurintysIViduriPinaiDydis, INPUT);
 	suteiktiPinModeMasyvui(ziurintysIDesinePinai,  ziurintysIDesinePinaiDydis, INPUT);
+}
+
+void suteiktiPinModeMasyvui(const int pinuMasyvas[], int masyvoDydis, int pinModas) {
+	for (int i = 0; i < masyvoDydis; i++) {
+		const int pin = pinuMasyvas[i];
+		pinMode(pin, pinModas);
+	}
+}
+
+void paruostiGeneralConfiga() {
+	/**
+	 * CONFIG
+	 * Nustato variklių dažnį.
+	 * Jeigu šitų nėra, tai varikliai nevažiuoja.
+	 *
+	 * ką daro `analogWriteFrequency`?
+	 *
+	 * Man neveikia per `vscode`, meta errorus -
+	 * tikriausiai trūksta kažkokios bibliotekos.
+	 *
+	 * `Upload`inant su arduino IDE veikia.
+	*/
+	analogWriteFrequency(PWM2, 15000.0f); /** #BROKEN #CHECK */
+	analogWriteFrequency(PWM1, 15000.0f);
+
+	/** enable receiving I suppose?:D */
+	irrecv.enableIRIn();
+	// irrecv.blink13(true); /** signalizuotų, kai gaunam signalą */
+
+	/**
+	 * CONFIG
+	 * įjungti/išjungti strategijų nustatymą pulteliu
+	 */
+	// pulteliuNustatytiStrategijas();
+
+	MAIN_STRATEGY_STATE = 1;
+	START_STRATEGY_STATE = 1;
+
+	/**
+	 * CONFIG
+	 * #TEMP #DEV - nevykdom pradinės strategijos (įjungti production'ui!)
+	 */
+	// vykdytiStrategija(pradineStrategija);
 }
--- sioko.ino
+++ sioko.ino
@@ -20,53 +20,7 @@ void setup()
 {
 	Serial.begin(115200);
 
-	const byte outputPins[] = {
-			LEDas,
-			PWM1,
-			PWM2,
-			DIR1,
-			DIR2,
-	};
-
-	const byte inputPins[] = {LeftLine1, LeftLine2, RightLine1, RightLine2, Right1, Right2, Right3, Rightback,
-														Left1, Left2, Left3, Leftback, Middle1, Middle2, Middle3, START_MODULE};
-
-	for (const byte &pin : outputPins)
-	{
-		pinMode(pin, OUTPUT);
-	}
-
-	for (const byte &pin : inputPins)
-	{
-		pinMode(pin, INPUT);
-	}
-
-	/**
-	 * ką daro `analogWriteFrequency`?
-	 *
-	 * Man neveikia per `vscode`, meta errorus -
-	 * tikriausiai trūksta kažkokios bibliotekos.
-	 *
-	 * `Upload`inant su arduino IDE veikia.
-	*/
-	// analogWriteFrequency(PWM2, 15000.0f); /** #BROKEN #CHECK */
-	// analogWriteFrequency(PWM1, 15000.0f);
-
-	/** enable receiving I suppose?:D */
-	irrecv.enableIRIn();
-	// irrecv.blink13(true); /** signalizuotų, kai gaunam signalą */
-
-	pulteliuNustatytiStrategijas();
-
-	MAIN_STRATEGY_STATE = 1;
-	START_STRATEGY_STATE = 1;
-
-	lauktiKolPaleisimRobota();
-
-	/**
-	 * #TEMP #DEV - nevykdom pradinės strategijos (įjungti production'ui!)
-	 */
-	// vykdytiStrategija(pradineStrategija);
+	inicializuoti();
 }
 
 void loop()
