9b6e170497926e738e68df04da53ec3827f7167a
Kipras Melnikovas <kipras@kipras.org>
2019-09-10 17:45:14 +0300

    Create general pin array in `inicializuoti`
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- inicializuoti.ino
+++ inicializuoti.ino
@@ -12,3 +12,37 @@
 
 #include "inicializuoti.h"
 #include <assert.h>
+
+void inicializuoti() {
+	sukurtiBendraPinuMasyva();
+}
+
+void sukurtiBendraPinuMasyva() {
+	int bendrasIndex = 0;
+
+	/** perkopijuojam: */
+
+	/** dešinę */
+	for (int i = 0; i < ziurintysIDesinePinaiDydis; i++) {
+		pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[bendrasIndex][0] = ziurintysIDesineSensoriai[i][0];
+		pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[bendrasIndex][1] = ziurintysIDesineSensoriai[i][1];
+
+		bendrasIndex++;
+	}
+
+	/** vidurį */
+	for (int i = 0; i < ziurintysIViduriPinaiDydis; i++) {
+		pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[bendrasIndex][0] = ziurintysIViduriSensoriai[i][0];
+		pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[bendrasIndex][1] = ziurintysIViduriSensoriai[i][1];
+
+		bendrasIndex++;
+}
+
+	/** kairę */
+	for (int i = 0; i < ziurintysIKairePinaiDydis; i++) {
+		pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[bendrasIndex][0] = ziurintysIKaireSensoriai[i][0];
+		pinaiIrJuPasisukimoLaipsniaiKaireMinusaiDesinePliusai[bendrasIndex][1] = ziurintysIKaireSensoriai[i][1];
+
+		bendrasIndex++;
+	}
+}
