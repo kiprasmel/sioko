3b0caa5714e1ef995c259b188252a5d5488b2004
Kipras Melnikovas <kipras@kipras.org>
2020-01-15 00:08:16 +0200

    arSensoriaiJunginejasi.(cpp|h): Atnaujinti logiką ir sutvarkyt stuff
    Signed-off-by: Kipras Melnikovas <kipras@kipras.org>

--- src/e2e/arSensoriaiJunginejasi.cpp
+++ src/e2e/arSensoriaiJunginejasi.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <string>
+// #include <functional>
 
 #include "Arduino.h"
 #include "xstate.h"
@@ -9,35 +10,64 @@
 #include "pagalbines_funkcijos.h"
 
 xs::StateMachine sukurtiPinoMasina(std::string id) {
-	xs::StateMachine masina = {
-		.id = id,
-		.initial =  "nemato",
-		.states = {
-			{"nemato", { .on = { {"MATO", "mato"} } }},
-			{"mato", { .on = { {"NEMATO", "vel_nemato"} } }},
-			{"vel_nemato", { .on = {} } },
+	// xs::StateMachine masina = {
+	// 	.id = id,
+	// 	.state = { .value = "nemato" },
+	// 	.states = {
+	// 		{"nemato", { .on = { {"MATO", "mato"} } }},
+	// 		{"mato", { .on = { {"NEMATO", "vel_nemato"} } }},
+	// 		{"vel_nemato", { .on = {} } },
+	// 	}
+	// };
+
+	struct xs::StateMachine masina = {
+		id: id,
+		state: { value: "nemato" },
+		on: {},
+		states: {
+			{"nemato", { id: "", state: { value: "" }, on: { {"MATO",   "mato"} },       states: {} }},
+			{"mato",   { id: "", state: { value: "" }, on: { {"NEMATO", "vel_nemato"} }, states: {} }},
+			{"vel_nemato", { id: "", state: { value: "" }, on: { },                          states: {} }},
+			// {"mato", { on: { {"NEMATO", "vel_nemato"} } }},
+			// {"vel_nemato", { on: {} } },
 		}
 	};
 
 	return masina;
 }
 
-std::vector<Dezute> Dezute::sukurtiDezutesIsPinu(int pinai[], const int &pinuMasyvoDydis) {
-	std::vector<Dezute> dezutes(pinuMasyvoDydis);
+std::vector<Dezute*> Dezute::sukurtiDezutesIsPinu(int pinai[], const int &pinuMasyvoDydis) {
+// std::vector<Dezute*> Dezute::sukurtiDezutesIsPinu(int pinai[], const int &pinuMasyvoDydis, std::function<bool(int pinas)> arMatoTikrinimoFunkcija) {
+	std::vector<Dezute*> dezutes(pinuMasyvoDydis);
 
 	for (int i = 0; i < pinuMasyvoDydis; ++i) {
 		const int &pinas = pinai[i];
 
 		xs::StateMachine masina = sukurtiPinoMasina("pinas" + pinas);
-		xs::Interpreter *interpreteris = xs::interpret(masina);
+		// Serial.print("\nSukurti pino masina");
 
-		Dezute paruostaDezute = {
+		xs::Interpreter interpreteris = xs::interpret(masina)
+			/** TODO šitų nebeturėtų reikėt */
+			// .onStart([]() {})
+			// .onTransition([]() {})
+			// .onStop([]() {})
+		;
+
+		// Serial.print("\ninterpret");
+
+		interpreteris.start();
+		// Serial.print("\nstart");
+
+		Dezute* paruostaDezute = new Dezute({
 			.pinas = pinas,
 			.arBaige = false,
-			interpreteris = interpreteris
-		};
+			.interpreteris = interpreteris
+			// ,
+			// .arMatoTikrinimoFunkcija = arMatoTikrinimoFunkcija
+		});
 
 		dezutes[i] = paruostaDezute;
+		// Serial.flush();
 	}
 
 	return dezutes;
@@ -45,35 +75,67 @@ std::vector<Dezute> Dezute::sukurtiDezutesIsPinu(int pinai[], const int &pinuMas
 
 void arSensoriaiJunginejasi() {
 	Serial.print("\narSensoriaiJunginejasi:\n");
+	ledasOFF();
 
-	std::vector<Dezute> dezutes = Dezute::sukurtiDezutesIsPinu(visiPinai, visiPinaiDydis);
+	// std::vector<Dezute*> dezutes = Dezute::sukurtiDezutesIsPinu(visiPinai, visiPinaiDydis);
+	std::vector<Dezute*> dezutes = Dezute::sukurtiDezutesIsPinu(linijuSensoriai, linijuSensoriaiDydis);
+
+	// std::vector<Dezute*> dezutes = Dezute::sukurtiDezutesIsPinu(visiPinai, visiPinaiDydis, [](int pinas) { return arMato(pinas); });
+	// std::vector<Dezute*> linijuDezutes = Dezute::sukurtiDezutesIsPinu(linijuSensoriai, linijuSensoriaiDydis, [](int pinas) { return arMatoLinija(pinas); });
+
+	// dezutes.insert(dezutes.end(), linijuDezutes.begin(), linijuDezutes.end());
 
 	size_t kiekBaige = 0;
 
 	while (kiekBaige < dezutes.size()) {
-		for (Dezute &dezute : dezutes) {
+		// Serial.print("\nwhile");
+		for (Dezute *dezute : dezutes) {
+			// Serial.print("\nfor");
 			/** atnaujinam state'ą / būseną */
-			dezute.interpreteris->send(arMato(dezute.pinas) ? "MATO" : "NEMATO");
 
-			if (dezute.interpreteris->state->value != "vel_nebemato") {
+			// bool arPinasMato = dezute->arMatoTikrinimoFunkcija(dezute->pinas);
+			bool arPinasMato = arMatoLinija(dezute->pinas);
+
+			// Serial.print("\narPinasMato = ");
+			// Serial.print(arPinasMato);
+
+			std::string eventas = arPinasMato ? "MATO" : "NEMATO";
+			// Serial.print("\neventas = ");
+			// Serial.print(eventas.c_str());
+
+			dezute->interpreteris.send(eventas);
+			// Serial.print("\nsend");
+			Serial.print("\n");
+			Serial.print(dezute->interpreteris.stateMachine.state.value.c_str());
+			Serial.print(" ");
+			Serial.print(kiekBaige);
+
+			if (dezute->interpreteris.stateMachine.state.value != "vel_nemato") {
 				/** dar nebaigė */
 				continue;
 			}
 
 			/** baigė, tik nežinia, kelintą kartą */
-			if (dezute.arBaige == true) {
+			if (dezute->arBaige == true) {
 				/** jau atnaujinom `kiekBaige` */
 				continue;
 			}
 
 			/** baigė pirmą kartą! */
-			dezute.arBaige = true;
+			dezute->arBaige = true;
 			++kiekBaige;
 
 			Serial.print("\nBaige dar vienas pinas - ");
-			Serial.print(dezute.pinas);
+			Serial.print(dezute->pinas);
 			Serial.print(" ; kiekBaige = ");
 			Serial.print(kiekBaige);
+			Serial.flush();
 		}
+		Serial.flush();
 	}
+
+	Serial.print("\ndone arSensoriaiJunginejasi\n");
+	ledasON();
+
+	while (1) {};
 }
--- src/e2e/arSensoriaiJunginejasi.h
+++ src/e2e/arSensoriaiJunginejasi.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+// #include "functional"
 
 #include "xstate.h"
 
@@ -9,12 +10,13 @@ xs::StateMachine sukurtiPinoMasina(std::string id);
 struct Dezute {
 	int pinas;
 	bool arBaige;
-	xs::Interpreter *interpreteris;
+	xs::Interpreter interpreteris;
+	// std::function<bool(int pinas)> arMatoTikrinimoFunkcija = [](int pinas) -> bool {};
 
-	static std::vector<Dezute> sukurtiDezutesIsPinu(int pinai[], const int &pinuMasyvoDydis);
+	static std::vector<Dezute*> sukurtiDezutesIsPinu(int pinai[], const int &pinuMasyvoDydis);
+	// static std::vector<Dezute*> Dezute::sukurtiDezutesIsPinu(int pinai[], const int &pinuMasyvoDydis, std::function<bool(int pinas)> arMatoTikrinimoFunkcija);
 
 	~Dezute() {
-		delete this->interpreteris;
 	}
 };
 
